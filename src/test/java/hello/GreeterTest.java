47253580fe0ce0481a7b716acb2fbb2ce31ff7e8
sashank0202 <85094977+sashank0202@users.noreply.github.com>
2021-07-07 20:11:33 +0530

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -10,8 +10,18 @@ public class GreeterTest {
   private Greeter greeter = new Greeter();
 
   @Test
-  public void greeterSaysHello() {
+  public void greeterSaysHello1() {
     assertThat(greeter.sayHello(), containsString("Bye"));
+  }
+  
+    @Test
+  public void greeterSaysHello2() {
+    assertThat(greeter.sayHello(), containsString("sashank"));
+  }
+  
+    @Test
+  public void greeterSaysHello3() {
+    assertThat(greeter.sayHello(), containsString("sai"));
   }
 
 }
